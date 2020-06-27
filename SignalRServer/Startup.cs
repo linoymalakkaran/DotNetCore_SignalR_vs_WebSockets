5960bcbbb5b10ebe0e9dcca47cc3b7d12d9270aa
Linoy Pappachan Malakkaran <malakkaran.pappachan@maqta.ae>
2020-06-27 21:33:56 +0400

    signalR-final

---
+++ SignalRServer/Startup.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRServer.Hubs;
+
+namespace SignalRServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseCors(builder => builder
+               .WithOrigins("null")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials());
+
+            //app.UseSignalR(endpoints =>
+            //{
+            //    endpoints.MapHub<ChatHub>("/chatHub");
+            //});
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chathub");
+            });
+
+        }
+    }
+}
